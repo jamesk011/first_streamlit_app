e6e49a1dd49fea4204d7d394a8fb520f43889c19
jamesk011 <140984027+jamesk011@users.noreply.github.com>
2023-08-04 10:53:53 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,19 +24,26 @@ fruits_to_show = my_fruit_list.loc[fruit_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+#create the repeatable code block(called a function)
+def get_fruityvice_data(this_fruit_choice):
+       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +this_fruit_choice)
+       # commented out --streamlit.text(fruityvice_response.json())
+       # just writes the data to the screen
+       # normalize the data into readable format
+       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+       return fruityvice_normalized
+
+#New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
       fruit_choice = streamlit.text_input('What fruit would you like information about?')
       if not fruit_choice:
                                           streamlit.error("Please select a fruit to get information.")
       else:
-                                                fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-                                                # commented out --streamlit.text(fruityvice_response.json())
-                                                # just writes the data to the screen
-                                                # normalize the data into readable format
-                                                fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-                                                # places data into a table 
-                                                streamlit.dataframe(fruityvice_normalized)
+                                               
+                                                # places data into a table
+            back_from_function = get_fruityvice_data(fruit_choice)
+            streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 
